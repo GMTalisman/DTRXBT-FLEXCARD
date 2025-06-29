086840060f4920668c5dc3082d377f447a312109
gmtalisman <115041068+GMTalisman@users.noreply.github.com>
2025-06-28 21:08:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
 import os
 
-st.set_page_config(page_title="DTR Image Generator", layout="centered")
-st.title("🔐 DTR Image Generator")
+st.set_page_config(page_title="DTRXBT FLEX CARD", layout="centered")
+st.title("DTRXBT FLEX CARD")
 
 # Load template
 try:
