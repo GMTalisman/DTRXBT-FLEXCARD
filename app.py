759d0e231f0d95be290d1613cf7630adbbe75e15
gmtalisman <115041068+GMTalisman@users.noreply.github.com>
2025-06-28 21:06:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ if submitted:
     # ✅ Bottom Banner (Large Font)
     st.markdown(
         "<div style='background-color:#0E1117; padding:10px; border-radius:8px;'>"
-        "<p style='text-align:center; color:white; font-size:2.5em;'>📱 Long Press Image to Save to Photos</p>"
+        "<p style='text-align:center; color:white; font-size:1.5em;'>📱 Long Press → Save or Send </p>"
         "</div>",
         unsafe_allow_html=True
     )
